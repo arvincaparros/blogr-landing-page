a2ac19246bc1000f50cc7e50584d011ecd2a8869
arvin <arvin.caparros.code@gmail.com>
2022-01-16 15:26:59 +0800

    3rd commit

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
+// Init Target Element
 const arrows = document.querySelectorAll('.arrow');
+const menuButton = document.querySelector('.hamburger-button');
+const closeButton = document.querySelector('.close-button');
 
+// Loop Target Element
 arrows.forEach((arrow, i) => {
+  // Click event
   arrow.addEventListener('click', function() {
+    // Callback
     toggleMenu(i);
-    setTimeout(function() {
-      toggleMenu(i).remove();
-    }, 5000)
   });
 })
 
+// Toggle Event
 function toggleMenu(i) {
   const lists = document.querySelectorAll('.list-item');
+  // Loop
   lists.forEach((list, j) => {
     if(j === i) {
       list.classList.toggle('drop-down');
     }
   })
-  
 }
+
+// Hamburger menu event
+menuButton.addEventListener('click', function() {
+  document.querySelector('.mobile-navbar').style.right = '0px';
+})
+
+// Close menu event
+closeButton.addEventListener('click', function() {
+  document.querySelector('.mobile-navbar').style.right = '-325px';
+})
